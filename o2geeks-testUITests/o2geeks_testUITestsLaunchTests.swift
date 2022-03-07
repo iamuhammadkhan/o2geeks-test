5f8495857a931d79ba463c961b702834a47e546b
Muhammad Khan <imuhammad.khan@icloud.com>
2022-03-08 01:38:07 +0500

    Initial Commit

---
+++ o2geeks-testUITests/o2geeks_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  o2geeks_testUITestsLaunchTests.swift
+//  o2geeks-testUITests
+//
+//  Created by Muhammad Khan on 3/8/22.
+//
+
+import XCTest
+
+class o2geeks_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
